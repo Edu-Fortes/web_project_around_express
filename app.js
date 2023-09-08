b9321d0dbb9a4a7352cd74f2a2b003bdd191f0c8
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-09-07 23:42:04 -0300

    feat(api): add error message when acessing root

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const { PORT = 3000 } = process.env;
 const users = require("./routes/users");
 const cards = require("./routes/cards");
 
-app.use("/users", users);
-app.use("/cards", cards);
+app.get("/", (req, res) => {
+  res.status(404).send({ message: "A solicitação não foi encontrada" });
+});
+
+app.use("/", users);
+app.use("/", cards);
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
