b3e04ca73ba1a21f15ade87e9403902572ac547e
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-09-15 01:07:25 -0300

    config(db): add mongDB and mongoose as database

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 const app = express();
+const mongoose = require("mongoose");
+
 const { PORT = 3000 } = process.env;
 
 const users = require("./routes/users");
 const cards = require("./routes/cards");
+
+mongoose.connect("mongodb://localhost:27017/aroundb");
 
 app.get("/", (req, res) => {
   res.status(404).send({ message: "A solicitação não foi encontrada" });
