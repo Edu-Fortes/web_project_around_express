81221a8e6dc06d69eff5cc566a27bd009b549968
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-09-20 00:40:59 -0300

    refact(user): remove versionKey on POST request

--- models/card.js
+++ models/card.js
@@ -4,25 +4,28 @@ const userSchema = require("./user");
 
 const { Schema, model } = mongoose;
 
-const cardSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  link: {
-    type: String,
-    required: true,
-    owner: {
-      type: ObjectId,
+const cardSchema = new Schema(
+  {
+    name: {
+      type: String,
       required: true,
+      minlength: 2,
+      maxlength: 30,
     },
-    likes: [
-      {
-        type: userSchema,
-        default: undefined,
+    link: {
+      type: String,
+      required: true,
+      owner: {
+        type: ObjectId,
+        required: true,
       },
-    ],
+      likes: [
+        {
+          type: userSchema,
+          default: undefined,
+        },
+      ],
+    },
   },
-});
+  { versionKey: false }
+);
