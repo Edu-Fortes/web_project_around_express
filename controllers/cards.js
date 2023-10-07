31a3cd10172122d66bfcca69c0d50e83574d1d37
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-10-07 01:15:46 -0300

    error(cards): add error message on requests

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,21 +2,31 @@ const Card = require("../models/card");
 
 module.exports = {
   getCards: async (req, res) => {
-    const cards = await Card.find();
-    return res.send(cards);
+    try {
+      const cards = await Card.find().orFail(
+        () => new Error("File could not be found")
+      );
+      res.send(cards);
+    } catch (error) {
+      res.status(404).send({ message: error.message });
+    }
   },
   createCard: async (req, res) => {
     const { name, link } = req.body;
     try {
       const newCard = await Card.create({ name, link, owner: req.user._id });
-      return res.send(newCard);
+      res.send(newCard);
     } catch (err) {
       res.status(400).send(err.message);
     }
   },
   deleteCard: async (req, res) => {
     const { cardId } = req.params;
-    const deletedCard = await Card.findByIdAndDelete(cardId);
-    res.send(deletedCard);
+    try {
+      const deletedCard = await Card.findByIdAndDelete(cardId).orFail();
+      res.send(deletedCard);
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
   },
 };
